140e2c3357966bce152f7fba84a50ae6b53b7270
anders <anders@columbia.edu>
2011-12-22 23:41:19 -0500

    get_or_create_service

--- src/controller/petra_api_controller.erl
+++ src/controller/petra_api_controller.erl
@@ -1,23 +1,34 @@
 -module(petra_api_controller,[Req]).
 -compile(export_all).
 
+%% should these go in the model modules instead?
 get_service(ServiceName) ->
     hd(boss_db:find(service,[{name,'equals',ServiceName}])).
+
+get_or_create_service(ServiceName) ->
+    Count = boss_db:count(service,[{name,'equals',ServiceName}]),
+    if 
+	Count == 0 ->
+	    Service = service:new(id,ServiceName),
+	    {ok,SavedService} = Service:save(),
+	    {created,SavedService};
+	 Count > 0 ->
+	    {exists,get_service(ServiceName)}
+    end.
 
 get_item(ServiceName,ItemName) ->
     Service = get_service(ServiceName),
     hd(boss_db:find(item,[{name,'equals',ItemName},{service_id,'equals',Service:id()}])).
     
-
+%% main controller functions
 service('GET',[]) ->
     {output,"here"};
 service('GET',[Name]) ->
     Service = get_service(Name),
     {json,Service:display_data()};
 service('PUT',[Name]) ->
-    Service = service:new(id,Name),
-    {ok,SavedService} = Service:save(),
-    {json,[{service,SavedService}]};
+    {_Created,Service} = get_or_create_service(Name),
+    {json,[{service,Service}]};
 service('DELETE',[Name]) ->
     Service = get_service(Name),
     boss_db:delete(Service:id()),
@@ -35,7 +46,8 @@ service('DELETE',[Name,ItemName]) ->
     Item = get_item(Name,ItemName),
     boss_db:delete(Item:id()),
     {output, "ok"}.
-    
+
+%% an odd one out to handle "/"    
 index('GET',[]) ->
     Services = boss_db:find(service,[]),
     {json,[{services,Services}]}.
